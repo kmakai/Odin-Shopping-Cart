84f3851fc3b6752dc5962794e7518a373e08914a
kmakai <92133709+kmakai@users.noreply.github.com>
2022-10-01 08:43:54 -0400

    1 file modified, 1 added

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,11 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import App from './App.js';
 import Profile from './Profile';
+import Nav from "./components/Nav";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
-    <div>Header</div>
+      <Nav />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Profile" element={<Profile />} />
